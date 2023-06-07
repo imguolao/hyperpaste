ef91d0e2ff8eb0789f9a111bef337fca96efafd1
imguolao <guolao.mq@foxmail.com>
2023-06-08 00:24:29 +0800

    chore: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+            jsx: true,
+            tsx: true,
+        },
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'prettier',
+    ],
+    rules: {
+        // vue
+        'vue/no-unused-vars': 'error',
+        'vue/require-default-prop': 'off',
+        'vue/multi-word-component-names': 'off',
+
+        // ts
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+
+        // eslint
+        'eol-last': ['error', 'always'],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+}
